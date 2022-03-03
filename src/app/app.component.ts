9ed282368fa144d745f068fedeb589742d7cfa57
alikazmi0944 <razaali00786@gmail.com>
2022-03-03 19:58:22 +0530

    Development complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
+import { Clipboard } from '@angular/cdk/clipboard';
 import { Component } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
+import { MatSliderChange } from '@angular/material/slider';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'password-generator';
+
+  readonly color: ThemePalette = 'primary';
+
+  password: string = '';
+
+  sliderValue: number = 8;
+
+  includeNumber: boolean = true;
+  includeLetters: boolean = false;
+  includeSymbols: boolean = false;
+
+  private readonly _numbers = "0123456789";
+  private readonly _letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  private readonly _symbols = "./=],)}$#&*?@%{([-";
+
+  constructor(
+    private readonly _clipboard: Clipboard,
+    private readonly _snackBar: MatSnackBar
+  ) { }
+
+  readonly onSliderChange = (change: MatSliderChange) => {
+    this.sliderValue = change.value || 0;
+  }
+
+  readonly copyPassword = () => {
+    this._clipboard.copy(this.password);
+    this._snackBar.open('Password copied !', '', { duration: 1000 });
+  }
+
+  readonly generatePassword = () => {
+    if (this.disableGeneratePasswordBtn()) {
+      return;
+    }
+    this.password = '';
+    let collection = [];
+    if (this.includeNumber) {
+      collection.push(this._numbers);
+    }
+    if (this.includeLetters) {
+      collection.push(this._letters);
+    }
+    if (this.includeSymbols) {
+      collection.push(this._symbols);
+    }
+    for (let i = 0; i < this.sliderValue; i++) {
+      const pickCollection = Math.floor(Math.random() * collection.length);
+      const current = collection[pickCollection];
+      const chosenCharacter = current[Math.floor(Math.random() * current.length)];
+      this.password += chosenCharacter;
+    }
+  }
+
+  readonly disableGeneratePasswordBtn = () => {
+    return !(this.includeNumber || this.includeLetters || this.includeSymbols);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { ClipboardModule } from '@angular/cdk/clipboard';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatRippleModule } from '@angular/material/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ClipboardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatRippleModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
